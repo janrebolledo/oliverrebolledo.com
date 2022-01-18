b2390b99e660ea96510d5ec0fb6fdc6cbc5ba97e
janrebolledo <janrebolledo63@gmail.com>
2022-01-18 13:20:40 -0800

    Hover

--- components/Services.js
+++ components/Services.js
@@ -6,30 +6,38 @@ export default function Services() {
       <h2>WORK</h2>
       <div className="services-container">
         <div className="services-card">
-          <h4>PHOTOGRAPHY</h4>
-          <Link href="/work/photography">
-            <p className="button explore-button">EXPLORE</p>
-          </Link>
+          <div className="services-info">
+            <h4>PHOTOGRAPHY</h4>
+            <Link href="/work/photography">
+              <p className="button explore-button">EXPLORE</p>
+            </Link>
+          </div>
         </div>
         <div className="services-card">
-          <h4>GRAPHIC DESIGN</h4>
-          <Link href="/work/graphic-design">
-            <p className="button explore-button">EXPLORE</p>
-          </Link>
+          <div className="services-info">
+            <h4>GRAPHIC DESIGN</h4>
+            <Link href="/work/graphic-design">
+              <p className="button explore-button">EXPLORE</p>
+            </Link>
+          </div>
         </div>
         <div className="services-card">
-          <h4>SOCIAL MEDIA MANAGEMENT</h4>
-          <Link href="/work/social-media">
-            <p className="button explore-button">EXPLORE</p>
-          </Link>
+          <div className="services-info">
+            <h4>SOCIAL MEDIA MANAGEMENT</h4>
+            <Link href="/work/social-media">
+              <p className="button explore-button">EXPLORE</p>
+            </Link>
+          </div>
         </div>
         <div className="services-card">
-          <h4>???</h4>
-          <Link href="/work/">
-            <p className="button explore-button">
-              EXPLORE <span className="material-icons">lock</span>
-            </p>
-          </Link>
+          <div className="services-info">
+            <h4>???</h4>
+            <Link href="/work/">
+              <p className="button explore-button">
+                EXPLORE <span className="material-icons">lock</span>
+              </p>
+            </Link>
+          </div>
         </div>
       </div>
     </section>
