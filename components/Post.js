8a5107750cf856bc964536ea3e57fddaabcd9121
janrebolledo <janrebolledo63@gmail.com>
2022-03-28 22:28:34 -0700

    Simplified Up Code

--- components/Post.js
+++ components/Post.js
@@ -3,15 +3,13 @@ import Link from "next/link";
 export default function Post({ post }) {
   return (
     <Link href={`/projects/${post.slug}`} passHref>
-      <p tabIndex="0">
-        <div>
-          <img
-            src={post.frontmatter.cover_image}
-            alt=""
-            className="aspect-square hover:opacity-50"
-          />
-        </div>
-      </p>
+      <div tabIndex="0" className="hover:cursor-pointer">
+        <img
+          src={post.frontmatter.cover_image}
+          alt=""
+          className="aspect-square hover:opacity-50"
+        />
+      </div>
     </Link>
   );
 }
