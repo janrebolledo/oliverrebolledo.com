7e58e760519992ac24c177fef98396836fc228c7
janrebolledo <janrebolledo63@gmail.com>
2022-04-09 14:34:28 -0700

    Project Content Styling

--- components/Button.js
+++ components/Button.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 function Button({ link, buttonText, style, className }) {
   if (style === "red") {
     return (
-      <Link href={link}>
+      <Link href={link} passHref>
         <div
           className={
             "text-white text-xs flex items-center justify-center py-4 px-6 mt-2 mr-2 hover:bg-[#ff000080] transition-all w-max bg-red-700 cursor-pointer " +
@@ -19,7 +19,7 @@ function Button({ link, buttonText, style, className }) {
 
   if (style === "outline") {
     return (
-      <Link href={link}>
+      <Link href={link} passHref>
         <div
           className={
             "text-white text-xs flex items-center justify-center py-4 px-6 mt-2 mr-2 hover:bg-[#ffffff40] transition-all border-solid border-2 w-max border-white cursor-pointer " +
